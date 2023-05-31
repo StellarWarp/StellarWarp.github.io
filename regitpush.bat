2a7246ac703ff486ae12e23a8643a8604795f530
WarpSpace <lsxestelle@icloud.com>
2023-05-31 22:54:07 +0800

    1

---
+++ regitpush.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+set remote=origin
+set branch=master
+set retry_interval=300
+
+:retry
+git push %remote% %branch%
+
+if %errorlevel% neq 0 (
+    echo Push failed. Retrying in %retry_interval% seconds...
+    timeout /t %retry_interval%
+    goto retry
+)
+
+echo Push successful.
+
+endlocal
